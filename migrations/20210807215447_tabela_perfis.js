84406f9e9846dc1783164b5aac45cd335e2e0b69
Alexandre Florentino <kanemaki@gmail.com>
2021-08-07 22:40:22 +0100

    chore: add register  perfis on the project.

--- migrations/20210807215447_tabela_perfis.js
+++ migrations/20210807215447_tabela_perfis.js
@@ -4,6 +4,13 @@ exports.up = function(knex) {
       table.increments('id').primary()
       table.string('nome').notNull().unique()
       table.string('rotulo').notNull()
+  }).then(function (){
+      return knex('perfis').insert([
+          {nome: 'comum', rotulo: 'Comum'},
+          {nome: 'admin', rotulo: 'Administrador'},
+          {nome: 'master', rotulo: 'Master'},
+
+      ])
   })
 };
 
