4c2e2d460726d37b40f74971d82b2b5d787f356d
Alexandre Florentino <kanemaki@gmail.com>
2021-08-09 22:34:50 +0100

    feat: add test consultas using knex.

--- testes/consultas.js
+++ testes/consultas.js
@@ -1,6 +1,16 @@
 const db = require('../config/db')
 
-db('perfis')
-    .then(res => res.map(p => p.nome))
-    .then(nomes => console.log(nomes))
-    .finally(() => db.destroy())
+//  db('perfis')
+//    .then(res => res.map(p => p.nome))
+//    .then(nomes => console.log(nomes))
+//    .finally(() => db.destroy())
+
+
+//    db('perfis').select('nome', 'id')
+//        .then(res => console.log(res))
+//        .finally(() => db.destroy())
+
+    db.select('nome', 'id')
+        .from('perfis')
+        .then(res => console.log(res))
+        .finally(() => db.destroy())        
